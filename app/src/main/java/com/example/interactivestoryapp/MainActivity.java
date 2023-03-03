470cfbba536132608a611a48fdb1050cf52cd9ed
Manuvel Victor <81816979+ManuvelVictor@users.noreply.github.com>
2023-03-03 12:11:44 +0530

    Second Commit

--- app/src/main/java/com/example/interactivestoryapp/MainActivity.java
+++ app/src/main/java/com/example/interactivestoryapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.interactivestoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,12 +22,22 @@ public class MainActivity extends AppCompatActivity {
         nameField = findViewById(R.id.nameEditText);
         startbutton = findViewById(R.id.startButton);
 
-        startbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = nameField.getText().toString();
-                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
-            }
+        startbutton.setOnClickListener(v -> {
+            String name = nameField.getText().toString();
+//                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+
+            startStory(name);
         });
+    }
+
+    private void startStory(String name) {
+        Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+        getResources().getString(R.string.name);
+        intent.putExtra("Name", name);
+        if(!name.isEmpty()) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(MainActivity.this, "Name cannot be empty", Toast.LENGTH_LONG).show();
+        }
     }
 }
